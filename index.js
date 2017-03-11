022b407ef39a0695c436ad41dbd3f6235f862bd0
Maledor <josh.r.cameron@gmail.com>
2017-03-10 20:48:24 -0500

    Successfully implemented GET, PUT, DELETE, and POST routes for updating the foods database through the REST API

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var server = express();
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 8080;
 var mongoURI = process.env.mongoURI || require('./secrets').mongoURI;
+
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect(mongoURI);
 
@@ -11,16 +15,18 @@ var foodSchema = mongoose.Schema({
   price: Number,
   category: String,
   isGlutenFree: Boolean,
-  calories: Number
+  calories: Number,
+  type: String
 });
 
 var Food = mongoose.model('Food', foodSchema);
-//
+
 // var whiteBread = new Food({
 //   price: 3.99,
 //   category: "Bakery",
 //   isGlutenFree: false,
-//   calories: 1300
+//   calories: 1300,
+//   type: "whiteBread"
 // });
 //   whiteBread.save(function(err, data){
 //     if(err){
@@ -33,7 +39,8 @@ var Food = mongoose.model('Food', foodSchema);
 //   price: 4.99,
 //   category: "Bakery",
 //   isGlutenFree: false,
-//   calories: 1000
+//   calories: 1000,
+//   type: "honeyWheatBread"
 // });
 //   honeyWheatBread.save(function(err, data){
 //     if(err){
@@ -46,7 +53,8 @@ var Food = mongoose.model('Food', foodSchema);
 //   price: 5.99,
 //   category: "Canned Goods",
 //   isGlutenFree: false,
-//   calories: 30
+//   calories: 30,
+//   type: "soup"
 // });
 //   soup.save(function(err, data){
 //     if(err){
@@ -59,7 +67,8 @@ var Food = mongoose.model('Food', foodSchema);
 //   price: 0.25,
 //   category: "Produce",
 //   isGlutenFree: true,
-//   calories: 100
+//   calories: 100,
+//   type: "banana"
 // });
 //   banana.save(function(err, data){
 //     if(err){
@@ -68,10 +77,38 @@ var Food = mongoose.model('Food', foodSchema);
 //       console.log(data);
 //     }
 //   });
-
-// GET /animals
+//
+// GET /foods
 server.get('/foods', function(req, res){
   Food.find({}, function(err, documents){
+    if(err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(200).json({
+        foods: documents
+      });
+    }
+  });
+});
+//GET /foods/price/:dollarAmount
+server.get('/foods/price/:dollarAmount', function(req, res){
+  Food.find({price: req.params.dollarAmount}, function(err, documents){
+    if(err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(200).json({
+        foods: documents
+      });
+    }
+  });
+});
+//GET /foods/category/:categoryName
+server.get('/foods/category/:categoryName', function(req, res){
+  Food.find({category: req.params.categoryName}, function(err, documents){
     if(err){
       res.status(500).json({
         msg: err
@@ -96,4 +133,64 @@ server.get('/foods/:id', function(req, res){
       });
     }
   });
+});
+//POST /foods
+server.post('/foods', function(req, res){
+  var food = new Food(req.body);
+  food.save(function(err, document){
+    if(err){
+      res.status(500).json({
+        msg: err
+      });
+    } else{
+      res.status(201).json({
+        msg: 'Success'
+      });
+    }
+  });
+});
+//PUT /foods/:id
+server.put('/foods/:id', function(req, res){
+  Food.findOneAndUpdate({_id: req.params.id}, req.body, function(err, document){
+    if(err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(200).json({
+        msg: 'Successfully updated'
+      });
+    }
+  });
+});
+//DELETE /foods/:id
+server.delete('/foods/:id', function(req, res){
+  Food.remove({_id: req.params.id}, function(err, document){
+    if(err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(200).json({
+        msg: 'Successfully deleted'
+      });
+    }
+  });
+});
+server.delete('/foods/category/:category', function(req, res){
+  Food.remove({category: req.params.category}, function(err, document){
+    if(err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(200).json({
+        msg: 'Successfully deleted'
+      });
+    }
+  });
+});
+
+server.listen(port, function(){
+  console.log('Now listening on port...', port);
 });
